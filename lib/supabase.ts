c808a66f96fe9eeeeced4946f3309a9f2153ab6a
jevin <jevin.koh.jk@gmail.com>
2026-05-19 17:59:50 +0800

    Supabase SSR client

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,30 @@
-import { createClient } from '@supabase/supabase-js'
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+import { createBrowserClient, createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
 
-// Remember to import { supabase } from '@/lib/supabase' in other files to access this data
+
+//use this in pages and components ('use client' files)
+export function createClient() {
+  return function createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
+}
+
+//use for API routes and server components
+export async function createServerSupabaseClient() {
+  const cookieStore = await cookies()
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() { return cookieStore.getAll() },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options}) => 
+            cookieStore.set(name, value, options)
+          )
+        },
+      },
+    }
+  )
+}
