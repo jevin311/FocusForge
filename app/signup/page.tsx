e576de3007ae3692a758926eef501a5d313ed25d
jevin <jevin.koh.jk@gmail.com>
2026-05-20 21:03:04 +0800

    signup halfway

---
+++ app/signup/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+import { createClient } from '@/lib/supabase/client'
+import { useState } from 'react'
+import Link from 'next/link'
+import Button from '@/components/ui/Button'
+import Input from '@/components/ui/Input'
+
